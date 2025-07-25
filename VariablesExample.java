8c4a1881a9fe58a766bf6af5bfa65ea722d1fa61
prasertsil26 <nnnot26@gmail.com>
2025-07-25 11:58:21 +0700

    25/7

---
+++ VariablesExample.java
@@ -0,0 +1,19 @@
+public class VariablesExample{
+  public static void main(String [] arg) {
+    char colthingSize = 's'; 
+    int websiteVisitors = 0;
+    char studentGrade = 'A';
+    double StudentGPA = 4.0;
+    final double PI = 3.14159;
+    float annualExpenses = 15000.00f;
+    final int MONTHS_IN_YEAR = 12;
+    System.out.println("ClothingSi Sizx: " + colthingSize);
+    System.out.println("WebsiteVisi Visitors: " + websiteVisitors);
+    System.out.println("StudentGrad Grade: " + studentGrade);
+    System.out.println("Student GPA: " + studentGrade);
+    System.out.println("value of Pi: " + PI);
+    System.out.println("Annual Expenses: " + annualExpenses);
+    System.out.println("MoNTHS_IN_YEAR" + MONTHS_IN_YEAR);
+
+  }
+}
